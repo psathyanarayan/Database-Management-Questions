fff04aeb7f772cc08bffe2d65d45f1fa8740fee0
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2022-04-20 20:25:46 +0530

    Version 2

--- Question_1/q1.sql
+++ Question_1/q1.sql
@@ -6,11 +6,11 @@ CREATE TABLE department(dept_id varchar(6) PRIMARY KEY,dept_name VARCHAR(20),man
 CREATE TABLE works(emp_id INT,dept_id VARCHAR(20), FOREIGN KEY(emp_id) REFERENCES employee(emp_id), FOREIGN KEY(dept_id) REFERENCES department(dept_id));
 CREATE TABLE loc(location_id INT PRIMARY KEY NOT NULL,city VARCHAR(15),postal_code INT);
 
-insert into employee values(1,'arun',date('1996-01-01'),5000);
-insert into employee values(2,'Billy',date('1996-01-01'),5000);
-insert into employee values(3,'catherin',date('1996-01-01'),5000);
-insert into employee values(4,'david',date('1996-01-01'),5000);
-insert into employee values(5,'emily',date('1996-01-01'),5000);
+insert into employee values(1,'arun',date('1996-01-01'),12500);
+insert into employee values(2,'Billy',date('1996-01-01'),15000);
+insert into employee values(3,'catherin',date('1996-01-01'),23000);
+insert into employee values(4,'david',date('1996-01-01'),75000);
+insert into employee values(5,'PQR',date('1996-01-01'),5000);
 insert into employee values(6,'fill',date('1996-01-01'),5000);
 insert into employee values(7,'george',date('1996-01-01'),5000);
 insert into employee values(8,'henry',date('1996-01-01'),5000);
@@ -28,20 +28,18 @@ insert into department values('D7','Admin',7);
 insert into department values('D8','Production',8);
 insert into department values('D9','QA',9);
 insert into department values('D10','Accounts',10);
-insert into department values('D11','ABC',11);
-insert into department values('D12','ABC',12);
-insert into department values('D13','ABC',13);
+
 
 -- insert 10 values in works table
 insert into works values(1,'D1');
-insert into works values(2,'D2');
+insert into works values(1,'D2');
 insert into works values(3,'D3');
 insert into works values(4,'D2');
 insert into works values(5,'D4');
 insert into works values(6,'D2');
 insert into works values(7,'D1');
 insert into works values(8,'D1');
-insert into works values(9,'D3');
+insert into works values(1,'D3');
 insert into works values(10,'D9');
 
 -- insert 10 different values in loc table with indian cities
@@ -66,6 +64,26 @@ select * from loc;
 SELECT emp_id from department,works where department.dept_id = works.dept_id and department.dept_name = "ABC" order by emp_id desc;
 
 -- Display Find the number of employees working in the department where "john" and "arun" are working.
+SELECT COUNT(emp_id) from employee,works,department where department.dept_id = works.dept_id and employee.emp_id = works.emp_id and employee.name in ("john","arun") ;
 
-SELECT works.dept_id from employee,works,department where department.dept_id = works.dept_id and employee.emp_id = works.emp_id and employee.name = "john" or employee.name = "arun" ;
-SELECT COUNT(emp_id) from employee,works,department where employee.emp_id = works.emp_id and department.dept_id = works.dept_id and employee.emp_name = "john" and "arun";
+-- Display the details of employees whose salary is between 12000 and 25000.
+SELECT * FROM employee where employee.salary BETWEEN 12000 AND 25000;
+
+-- Display the details of department managed by the employee named "PQR‟.
+SELECT department.dept_id,dept_name,manager_id FROM employee,department,works where employee.emp_id = works.emp_id and works.dept_id = department.dept_id and employee.name = "PQR";
+
+-- Display the details of department where the maximum number of employees are working.
+CREATE VIEW count AS SELECT department.dept_id,dept_name,manager_id,count(employee.emp_id) as cn FROM employee,department,works where employee.emp_id = works.emp_id and works.dept_id = department.dept_id group by department.dept_id,dept_name,manager_id order by COUNT(employee.emp_id) desc;
+SELECT * FROM count;
+SELECT dept_id,dept_name,manager_id FROM count WHERE cn =  (SELECT MAX(cn) FROM count group by dept_id ORDER BY cn DESC LIMIT 1);
+
+-- Create a package with functions and procedures to do the following
+-- Find the number of employees whose salary is greater than the average salary using procedures.
+DELIMITER //
+CREATE PROCEDURE avg_salary()
+BEGIN
+    SELECT AVG(salary) FROM employee;
+END //
+DELIMITER ;
+
+CALL avg_salary();
